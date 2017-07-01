28199013db2ca2ea6ddc393bb2e79dd2dff83244
ferventdev <alex.4everyours@gmail.com>
2017-07-01 17:18:07 +0300

    t01/CrazyLogger class modified: fields and constructor added

--- src/main/java/t01/CrazyLogger.java
+++ src/main/java/t01/CrazyLogger.java
@@ -1,7 +1,31 @@
 package t01;
 
+import java.util.Locale;
+
 /**
  * Created by Aleksandr Shevkunenko on 01.07.2017.
  */
 public class CrazyLogger {
+    private final StringBuilder storage = new StringBuilder();
+    private Locale loggerLocale;
+
+    public CrazyLogger(Locale loggerLocale) {
+        this.loggerLocale = loggerLocale;
+    }
+
+    public CrazyLogger() {
+        this.loggerLocale = Locale.getDefault();
+    }
+
+    public Locale getLoggerLocale() {
+        return loggerLocale;
+    }
+
+    public void setLoggerLocale(Locale loggerLocale) {
+        this.loggerLocale = loggerLocale;
+    }
+
+    public void log(String message) {
+
+    }
 }
